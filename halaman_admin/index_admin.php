d5798ab77c12315ac8d4c943a0ce0a9dfc8f9ba6
IhsanRizaldi <taufanpa222@gmail.com>
2023-07-13 20:45:36 +0700

    Config Admin Page

--- halaman_admin/index_admin.php
+++ halaman_admin/index_admin.php
@@ -12,6 +12,37 @@
             endif;
         ?>
             <h1 class="text-center">Selamat Datang Admin</h1>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card text-center">
+                        <div class="card-body"> 
+                            <h4>Jumlah Category Dibuat</h4>
+                            <?php
+                                require '../koneksi.php';
+                                $sql = mysqli_query($conn,"SELECT count(*) as total FROM category");
+                                $row = mysqli_fetch_assoc($sql);
+                            ?>
+                            <h5><?php echo $row['total'] ?></h5>
+                            <a href="template_admin.php?halaman=index_article" class="btn btn-primary">Category</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card text-center">
+                        <div class="card-body"> 
+                            <h4>Jumlah Article Dibuat</h4>
+                            <?php
+                                require '../koneksi.php';
+                                $sql = mysqli_query($conn,"SELECT count(*) as total FROM article");
+                                $row = mysqli_fetch_assoc($sql);
+                            ?>
+                            <h5><?php echo $row['total'] ?></h5>
+                            <a href="template_admin.php?halaman=index_article" class="btn btn-primary">Article</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     </div>
 </div>
